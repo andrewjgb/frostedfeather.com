38b8d43ea3f60f16f950a0a470065bf4fc743672
andrewjgb <andrew@chiprate.co.uk>
2014-12-08 11:59:56 +0000

    Removal of svgstore
    Removal of svgstore from grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,18 +83,7 @@ module.exports = function (grunt) {
 		  }
 		}
 		
-		  svgstore: {
-    options: {
-      prefix : 'icon-', // This will prefix each ID
-      svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
-        viewBox : '0 0 100 100',
-        xmlns: 'http://www.w3.org/2000/svg'
-      }
-    },
-    your_target: {
-      // Target-specific file lists and/or options go here.
-    },
-  }
+		
 		
 	  },	
 
